125e4a2122d31ae36d17d69838ea1c93e11d21c8
Daniel Albano III <captdane3@gmail.com>
2023-09-16 01:55:05 +0800

    ongoing pawn object development

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const board = document.createElement("div")
+const cells = []
 board.classList.add("board")
 document.body.appendChild(board)
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
+for (let i = 1; i <= 8; i++) {
+  for (let j = 1; j <= 8; j++) {
     const space = document.createElement("div")
     space.classList.add("space")
-    space.dataset.row = i + 1
-    space.dataset.col = j + 1
+    space.dataset.row = i
+    space.dataset.col = j
+    const cell = {
+      row: i,
+      col: j,
+      div: space
+    }
     if (j % 2 === 0) {
       space.classList.add(switchColor(i)[0])
     } else {
       space.classList.add(switchColor(i)[1])
     }
     board.appendChild(space)
+    cells.push(cell)
   }
 }
 const spaces = document.querySelectorAll(".space")
@@ -34,48 +41,75 @@ class Pawn{
     this.col = col
     this.color = color
     this.kind = "pawn"
-    this.moves = this.moves(color)
+    this.moves = this.allmoves()
     this.nowjumped = false
+    this.piece = this.render()
+    this.moveTo(row,col)
   }
 
-  moves(color){
-    const allMoves = [
-      [//[0] BLACK
-        {
-          jump: [2,0],
-          forward: [1,0],
-          leftatk: [1,-1],  //enpassant L
-          rightatk: [1,1]   //enpassant R
-        },
-      
-      //[1] WHITE
-        {
-          jump: [-2,0],
-          forward: [-1,0],
-          leftatk: [-1,-1], //enpassant L
-          rightatk: [-1,1]  //enpassant R
-        }
-      ]
-    ]
+  render(){
+    const piece = document.createElement('div')
+    piece.dataset.piece = this.kind
+    piece.dataset.color = this.color
+    piece.classList.add('piece')
+    return piece
+  }
 
-    let moves
-    if (color === 'white'){
-      moves = allMoves[1]
-    } else {
-      moves = allMoves[0]
+  allmoves(){
+    const x = this.color === 'white' ? -1 : 1
+    const moves = {
+      jump: [2*x,0],
+      forward: [1*x,0],
+      leftatk: [1*x,-1],  //enpassant L
+      rightatk: [1*x,1]   //enpassant R
     }
-    
     return moves
+  }
+
+  showMoves(){
+    const moves = this.moves
+
+    const forward = moves.forward
+    const forwardcell = cells.find(cell => 
+      cell.row == forward[0] + this.row && cell.col == forward[1] + this.col)
+
+    const jump = moves.jump
+    const jumpcell = cells.find(cell => 
+      cell.row == jump[0] + this.row && cell.col == jump[1] + this.col)
+
+    const leftatk = moves.leftatk
+    const leftatkcell = cells.find(cell =>
+      cell.row == leftatk[0] + this.row && leftatk[1] + this.col)
+
+    const rightatk = moves.rightatk
+    const rightatkcell = cells.find(cell =>
+      cell.row == rightatk[0] + this.row && rightatk[1] + this.col)
+    
+    return {
+      step: forwardcell,
+      hop: jumpcell,
+      L: leftatkcell,
+      R: rightatkcell
+    }
   }
 
   jumped() {
     this.nowjumped = true
   }
+
+  moveTo(row,col){
+    const cell = cells.find(cell => cell.row == row && cell.col == col)
+    const space = cell.div
+    space.appendChild(this.piece)
+    this.row = row
+    this.col = col
+  }
 }
 
-const p1w = new Pawn(7,1,"")
+const p1w = new Pawn(7,5,"white")
 
-console.log(p1w)
+console.log(p1w.moves)
 console.log(p1w.nowjumped)
-p1w.jumped()
-console.log(p1w.nowjumped)
+console.log(p1w.showMoves())
+
+console.log(cells)
